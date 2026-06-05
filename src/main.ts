01b2d6a7bb3197359dc7530edfd3dc38f9bcbc2e
sergey-uhanov <uhanov.sergio@gmail.com>
2026-06-05 13:54:40 +0300

    add port

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
 
   app.enableCors();
   app.useWebSocketAdapter(new WsAdapter(app));
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
