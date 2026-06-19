ec0c1a8c439cebe15b1f51d1a6b372fc54756ef2
sergey-uhanov <uhanov.sergio@gmail.com>
2026-06-19 21:36:53 +0300

    bugfix

--- src/device-control/handlers/register.handler.ts
+++ src/device-control/handlers/register.handler.ts
@@ -1,10 +1,12 @@
 import {Injectable} from "@nestjs/common";
 import {ClientRegistryService} from "../services/client-registry.service";
 import {WebSocket} from "ws";
+import {CommandRouterService} from "../services/command-router.service";
 @Injectable()
 export class RegisterHandler {
     constructor(
         private readonly registry: ClientRegistryService,
+        private readonly commandRouterService: CommandRouterService,
     ) {}
 
     handle(ws: WebSocket, data: any) {
@@ -15,5 +17,8 @@ export class RegisterHandler {
             socket: ws,
             lastSeen: Date.now(),
         });
+        switch (data.type){
+            case 'esp-32' : this.commandRouterService.send('frontend-admin', {type: 'esp32 is online'})
+        }
     }
 }
